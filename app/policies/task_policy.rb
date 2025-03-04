c4d49a4e59a04adf0019be3ed46b2c72a8155bd1
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-04 15:34:57 -0300

    feat: added task policy

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,6 +1,24 @@
 class TaskPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.narrator?
+        scope.all
+      else
+        scope.where(character: user)
+      end
+    end
+  end
+
   def create?
     user.character?
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    user.narrator? || record.character == user
   end
 
   def update?
