63c336e83ad8411b7a31857ae4d568b045723926
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-04 15:33:59 -0300

    feat: added mission policy

--- app/policies/mission_policy.rb
+++ app/policies/mission_policy.rb
@@ -1,4 +1,22 @@
 class MissionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.narrator?
+        scope.all
+      else
+        scope.where(character: user)
+      end
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    user.narrator? || record.character == user
+  end
+
   def create?
     user.narrator?
   end
