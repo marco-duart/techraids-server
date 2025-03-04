e9201b4eedc0371babf4260d433579132f2aee6a
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-04 15:35:26 -0300

    feat: updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  # mount_devise_token_auth_for 'User', at: 'auth'
-  namespace :api do
-    namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth"
+  mount_devise_token_auth_for "User", at: "auth"
 
-      get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
 
-      resources :tasks, only: [ :index, :show, :create, :update, :destroy ]
-      resources :missions, only: [ :index, :show, :create, :update, :destroy ]
-      resources :guilds, only: [ :index, :show, :create, :update, :destroy ]
-      resources :quests, only: [ :index, :show, :create, :update, :destroy ]
-      resources :characters, only: [ :index, :show, :update ]
-      resources :treasure_chests, only: [ :index, :show ]
-    end
-  end
+  resources :tasks, only: [ :index, :show, :create, :update, :destroy ]
+  resources :missions, only: [ :index, :show, :create, :update, :destroy ]
+  resources :guilds, only: [ :index, :show, :create, :update, :destroy ]
+  resources :quests, only: [ :index, :show, :create, :update, :destroy ]
+  resources :characters, only: [ :index, :show, :update ]
+  resources :treasure_chests, only: [ :index, :show ]
 end
