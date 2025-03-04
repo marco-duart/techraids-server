92f65d907194de81711e3acffcd1c80e111b5d38
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-04 15:30:02 -0300

    feat: updated device config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include Pundit::Authorization
+  include Devise::Controllers::Helpers
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :disable_session
+
+  private
+
+  def disable_session
+    request.session_options[:skip] = true
+  end
 end
