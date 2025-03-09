75fcb5e65b5fce8a11952570893ee0840fa267b5
marco-duart <marcoaurelio_db@hotmail.com>
2025-03-09 17:39:03 -0300

    feat: updated character class flow

--- app/models/character_class.rb
+++ app/models/character_class.rb
@@ -2,4 +2,8 @@ class CharacterClass < ApplicationRecord
   belongs_to :specialization
   has_many :users
   has_one_attached :image
+
+  def as_json(options = {})
+    super(options).merge(image_url: image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil)
+  end
 end
