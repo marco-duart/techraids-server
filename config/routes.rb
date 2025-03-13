8e4ca1b1e65f8d7fa262cb5c66951416d26311b6
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-13 17:31:58 -0300

    feat: updated route

--- config/routes.rb
+++ config/routes.rb
@@ -11,14 +11,17 @@ Rails.application.routes.draw do
   resources :quests, only: [ :index, :show, :create, :update, :destroy ]
   resources :chapters, only: [ :index, :show, :create, :update, :destroy ]
   resources :villages, only: [ :index, :show, :create, :update, :destroy ]
-  resources :characters, only: [ :index, :show, :update ]
   resources :treasure_chests, only: [ :index, :show ]
   resources :specializations, only: [ :index, :show, :create, :update, :destroy ]
   resources :character_classes, only: [ :index, :show, :create, :update, :destroy ]
 
-  namespace :characters do
-    post "select_specialization", to: "characters#select_specialization"
-    post "switch_character_class", to: "characters#switch_character_class"
-    get "character_quest", to: "characters#character_quest"
+  # Rotas com regras de negócio para narrator
+  # scope :narrator, controller: :narrator do
+
+  # end
+  scope :characters, controller: :characters do
+    post "select_specialization", action: :select_specialization
+    post "switch_character_class", action: :switch_character_class
+    get "character_quest", action: :character_quest
   end
 end
