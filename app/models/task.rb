6f9bb1ebd98d1a8dcfe0d5a6d33d9aa640aacc4d
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-14 16:48:17 -0300

    feat: added scope to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   belongs_to :character, class_name: "User"
   belongs_to :chapter
   belongs_to :narrator, class_name: "User"
+
+  enum :status, { pending: 0, approved: 1, rejected: 2 }
+
+  scope :last_pending, -> { pending.order(created_at: :desc).first }
 end
