40cd6416cf6d81885ed2cd44fb28fd42ca0849db
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-04-15 17:32:42 -0300

    feat: added ranking route

---
+++ app/services/characters_ranking_service.rb
@@ -0,0 +1,57 @@
+class CharactersRankingService
+  def initialize(user)
+    @user = user
+    @guild = user.guild
+  end
+
+  def guild_ranking
+    return { success: false, error: "Personagem não pertence a nenhuma guild!" } unless @guild
+
+    characters = @guild.characters
+
+    {
+      success: true,
+      data: {
+        missions_completed: ranking_by_missions_completed(characters),
+        tasks_completed: ranking_by_tasks_completed(characters),
+        gold_earned: ranking_by_gold_earned(characters),
+        experience: ranking_by_experience(characters)
+      }
+    }
+  end
+
+  private
+
+  def ranking_by_missions_completed(characters)
+    characters.left_joins(:character_missions)
+              .where(missions: { status: :approved })
+              .group("users.id")
+              .order("COUNT(missions.id) DESC")
+              .limit(10)
+              .pluck("users.nickname, COUNT(missions.id) as count")
+  end
+
+  def ranking_by_tasks_completed(characters)
+    characters.left_joins(:character_tasks)
+              .where(tasks: { status: :approved })
+              .group("users.id")
+              .order("COUNT(tasks.id) DESC")
+              .limit(10)
+              .pluck("users.nickname, COUNT(tasks.id) as count")
+  end
+
+  def ranking_by_gold_earned(characters)
+    characters.left_joins(:character_missions)
+              .where(missions: { status: :approved })
+              .group("users.id")
+              .order("SUM(missions.gold_reward) DESC")
+              .limit(10)
+              .pluck("users.nickname, SUM(missions.gold_reward) as total_gold")
+  end
+
+  def ranking_by_experience(characters)
+    characters.order(experience: :desc)
+              .limit(10)
+              .pluck(:nickname, :experience)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for "User", at: "auth", controllers: {
-    sessions: "custom_sessions"
+    sessions: "custom_sessions",
+    token_validations: "custom_token_validations"
   }
 
   get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :public do
+    collection do
+      get "guilds", to: "guilds#simple_list"
+    end
+  end
 
   resources :tasks, only: [ :index, :show, :create, :update, :destroy ]
   resources :missions, only: [ :index, :show, :create, :update, :destroy ]
@@ -21,7 +28,8 @@ Rails.application.routes.draw do
   # end
   scope :characters, controller: :characters do
     post "select_specialization", action: :select_specialization
-    post "switch_character_class", action: :switch_character_class
+    post "switch_class", action: :switch_character_class
     get "character_quest", action: :character_quest
+    get "ranking", action: :ranking
   end
 end
