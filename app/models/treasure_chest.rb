2f95a1ae5324a1a13c57ce55851f1cd4d69985cf
marco-duart <marcoaurelio_db@hotmail.com>
2025-04-20 14:49:54 -0300

    fat: added new relation

--- app/models/treasure_chest.rb
+++ app/models/treasure_chest.rb
@@ -1,4 +1,11 @@
 class TreasureChest < ApplicationRecord
+  belongs_to :guild
+  has_many :rewards
   has_many :character_treasure_chests
   has_many :characters, through: :character_treasure_chests, source: :character
+
+  scope :active, -> { where(active: true) }
+  scope :with_available_rewards, -> {
+    joins(:rewards).merge(Reward.available).distinct
+  }
 end
