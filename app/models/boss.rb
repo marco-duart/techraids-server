5fdc8f18e767e327b75932ebe132af251d6bd8f1
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-05-28 13:05:41 -0300

    feat: updated boss methods and relations

--- app/models/boss.rb
+++ app/models/boss.rb
@@ -1,8 +1,39 @@
 class Boss < ApplicationRecord
   belongs_to :chapter
+  belongs_to :finishing_character, class_name: "User", optional: true
+  has_one :quest, through: :chapter
+  has_one :guild, through: :quest
   has_one_attached :image
 
   def as_json(options = {})
     super(options).merge(image_url: image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil)
+  end
+
+  def defeat_threshold
+    team_members = guild.characters
+    team_size = team_members.count
+
+    base_experience = chapter.required_experience * 1.2
+
+    additional_members = [ team_size - 1, 0 ].max
+    additional_experience = chapter.required_experience * 0.95 * additional_members
+
+    (base_experience + additional_experience).round
+  end
+
+  def team_can_defeat?
+    team_members = guild.characters.where(current_chapter_id: chapter.id)
+    total_experience = team_members.sum(:experience)
+
+    total_experience >= defeat_threshold
+  end
+
+  def finishing_hero?(character)
+    return false unless team_can_defeat?
+
+    guild.characters
+         .where(current_chapter_id: chapter.id)
+         .order(experience: :desc)
+         .first == character
   end
 end
