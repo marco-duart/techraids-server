aa07dcb62f3254a1a233a0e7d7c0a174bf7d6bd1
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-05-28 13:06:59 -0300

    feat: added validations in chapter

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -3,4 +3,9 @@ class Chapter < ApplicationRecord
   has_many :tasks
   has_many :missions
   has_one :boss
+
+  validates :position, presence: true
+  validates :position, uniqueness: { scope: :quest_id }
+
+  scope :ordered, -> { order(:position) }
 end
