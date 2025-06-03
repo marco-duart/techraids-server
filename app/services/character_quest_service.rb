1cce3d5a594e476706a41d4253929f161a573880
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-06-03 16:54:40 -0300

    feat: updated character quest service

--- app/services/character_quest_service.rb
+++ app/services/character_quest_service.rb
@@ -3,31 +3,31 @@ class CharacterQuestService
 
   def initialize(user)
     @user = user
+    @guild = user.guild
   end
 
   def fetch_quest_and_companions
-    return { success: false, error: "Personagem não pertence a nenhuma guild!" } if @user.guild.nil?
+    return { success: false, error: "Personagem não pertence a nenhuma guild!" } if @guild.nil?
 
-    guild = @user.guild
-    quest = guild.quest
-    chapters = fetch_chapters_with_bosses(quest.chapters.includes(boss: { finishing_character: :character_class }))
-    current_chapter = @user.current_chapter
-    current_boss = current_chapter.boss
-    guild_members = fetch_guild_members(guild)
+    quest = @guild.quest
+    chapters = quest.chapters
+                   .includes(boss: { finishing_character: :character_class })
+                   .ordered
 
-    boss_data = current_boss&.as_json&.merge(
-                  team_can_defeat: current_boss.team_can_defeat?,
-                  is_finishing_hero: current_boss.finishing_hero?(@user)
-                )
+    guild_members = @guild.characters
+                          .includes(:character_class, :current_chapter, :active_title)
+                          .where.not(id: @user.id)
+                          .group_by(&:current_chapter_id)
+
+    processed_chapters = chapters.map do |chapter|
+      process_chapter(chapter, guild_members[chapter.id] || [])
+    end
 
     {
       success: true,
       data: {
         quest: quest,
-        chapters: chapters,
-        current_chapter: current_chapter,
-        current_boss: boss_data,
-        guild_members: guild_members,
+        chapters: processed_chapters,
         last_task: fetch_last_task,
         last_mission: fetch_last_mission
       }
@@ -36,53 +36,61 @@ class CharacterQuestService
 
   private
 
-  def fetch_chapters_with_bosses(chapters)
-    chapters.ordered.map do |chapter|
-      chapter_data = chapter.as_json
+  def process_chapter(chapter, chapter_members)
+    is_hero_chapter = chapter == @user.current_chapter
 
-      if chapter.boss.present?
-        boss = chapter.boss
-        boss_data = boss.as_json
-
-        if boss.finishing_character.present?
-          finishing = boss.finishing_character
-          boss_data[:finishing_character] = {
-            id: finishing.id,
-            nickname: finishing.nickname,
-            image_url: finishing.character_class.image.attached? ? rails_blob_url(finishing.character_class.image) : nil
-          }
-        end
-
-        chapter_data[:boss] = boss_data
-      end
-
-      chapter_data
-    end
+    {
+      **chapter.as_json,
+      is_hero_chapter: is_hero_chapter,
+      character: is_hero_chapter ? format_character_data(@user) : nil,
+      guild_members: chapter_members.any? ? chapter_members.map { |m| format_character_data(m) } : nil,
+      boss: process_boss(chapter.boss, is_hero_chapter)
+    }
   end
 
-  def fetch_guild_members(guild)
-    guild.characters.where.not(id: @user.id).select(:id, :nickname, :experience, :character_class_id, :current_chapter_id, :active_title_id).map do |member|
+  def process_boss(boss, is_hero_chapter)
+    return nil unless boss
+
+    finishing_character = boss.finishing_character
+    finishing_data = if finishing_character&.character_class
       {
-        id: member.id,
-        nickname: member.nickname,
-        current_level: member.current_level,
-        experience: member.experience,
-        character_class: {
-          name: member.character_class.name,
-          slogan: member.character_class.slogan,
-          image_url: member.character_class.image.attached? ? rails_blob_url(member.character_class.image) : nil
-        },
-        current_chapter: member.current_chapter,
-        active_title: member.active_title
+        id: finishing_character.id,
+        nickname: finishing_character.nickname,
+        image_url: finishing_character.character_class.image.attached? ?
+                   rails_blob_url(finishing_character.character_class.image) : nil
       }
     end
+
+    {
+      **boss.as_json,
+      finishing_character: finishing_data,
+      team_can_defeat: boss.team_can_defeat?,
+      is_finishing_hero: is_hero_chapter ? boss.finishing_hero?(@user) : nil
+    }
+  end
+
+  def format_character_data(character)
+    character_class = character.character_class
+    {
+      id: character.id,
+      nickname: character.nickname,
+      current_level: character.current_level,
+      experience: character.experience,
+      character_class: {
+        name: character_class.name,
+        slogan: character_class.slogan,
+        image_url: character_class.image.attached? ? rails_blob_url(character_class.image) : nil
+      },
+      current_chapter: character.current_chapter,
+      active_title: character.active_title
+    }
   end
 
   def fetch_last_task
-    @user.character_tasks.last_pending || @user.tasks.last
+    @last_task ||= @user.character_tasks.last_pending || @user.tasks.last
   end
 
   def fetch_last_mission
-    @user.character_missions.last_pending || @user.missions.last
+    @last_mission ||= @user.character_missions.last_pending || @user.missions.last
   end
 end
