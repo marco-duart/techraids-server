9bae4d697bf140092db1d00f87beb55eca4a58f1
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-06-06 16:23:42 -0300

    feat: added narrator policy

---
+++ app/policies/narrator_policy.rb
@@ -0,0 +1,12 @@
+class NarratorPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def performance_report?
+    user.narrator? && user.managed_guild.present?
+  end
+end
