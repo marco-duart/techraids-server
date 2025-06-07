e3d72364185758b829e2c068ef825c87c5f58040
marco-duart <marcoaurelio_db@hotmail.com>
2025-06-07 11:46:26 -0300

    feat: added timezone

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module Techraids
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Brasilia" # 'Brasilia' # ou 'America/Sao_Paulo'
+    config.active_record.default_timezone = :utc
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
