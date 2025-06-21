0c184bdb0930c9d5573f80f896f601a83f795698
marco-duart <marcoaurelio_db@hotmail.com>
2025-06-21 13:50:13 -0300

    feat: updated guild relations

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -3,5 +3,6 @@ class Guild < ApplicationRecord
   belongs_to :narrator, class_name: "User"
   has_many :characters, class_name: "User", dependent: :nullify
   has_many :specializations, dependent: :destroy
+  has_many :treasure_chests, dependent: :destroy
   has_one :quest, dependent: :destroy
 end
