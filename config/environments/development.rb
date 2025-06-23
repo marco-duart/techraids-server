cbf2b2cb54c8a9d5deead29eab286e3b836e091a
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-06-23 10:59:03 -0300

    feat: forcing any origin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << /.*\.ngrok-free\.app/
+  # config.hosts << /.*\.ngrok-free\.app/
+  config.hosts << /.*/
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
