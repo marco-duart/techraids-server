6b1131e57248d6439efa40f1bb51c9c3037a8e2e
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-06-23 08:41:09 -0300

    feat: allow vercel apps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
-  config.hosts << "techraids-client.vercel.app"
+  config.hosts << ".vercel.app"
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
