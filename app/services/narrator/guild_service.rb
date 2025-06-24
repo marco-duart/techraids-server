82282aab014693174498aa9e7a4b1f1b23422dc8
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-06-24 17:25:01 -0300

    feat: updated guild service

--- app/services/narrator/guild_service.rb
+++ app/services/narrator/guild_service.rb
@@ -26,10 +26,43 @@ module Narrator
       }
     end
 
+    def pending_rewards
+      authorize_pending_rewards
+
+      guild = @user.managed_guild
+      pending_bosses = guild.quest.chapters.joins(:bosses)
+                           .where(bosses: { defeated: true, reward_claimed: false })
+      pending_chests = CharacterTreasureChest.joins(:character)
+                                             .where(characters: { guild_id: guild.id }, reward_claimed: false)
+
+      {
+        success: true,
+        message: "Pendências de rewards listadas com sucesso.",
+        data: {
+          pending_bosses: pending_bosses,
+          pending_chests: pending_chests
+        }
+      }
+    rescue Pundit::NotAuthorizedError
+      {
+        success: false,
+        message: "Você não tem autorização para acessar este recurso."
+      }
+    rescue => e
+      {
+        success: false,
+        message: "Ocorreu um erro ao acessar esse recurso: #{e.message}"
+      }
+    end
+
     private
 
     def authorize_guild_members
       raise Pundit::NotAuthorizedError unless NarratorPolicy.new(@user, nil).guild_members?
+    end
+
+    def authorize_pending_rewards
+      raise Pundit::NotAuthorizedError unless NarratorPolicy.new(@user, nil).pending_rewards?
     end
   end
 end
