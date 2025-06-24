67fd8b235b457febf08b13799aa3a84944e34358
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-06-24 17:25:41 -0300

    feat: updated character treasure chest

--- db/migrate/20250228180220_create_character_treasure_chests.rb
+++ db/migrate/20250228180220_create_character_treasure_chests.rb
@@ -1,6 +1,7 @@
 class CreateCharacterTreasureChests < ActiveRecord::Migration[8.0]
   def change
     create_table :character_treasure_chests do |t|
+      t.boolean :reward_claimed, default: false
       t.timestamps
     end
   end
