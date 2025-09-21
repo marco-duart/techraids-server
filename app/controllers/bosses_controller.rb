6906a274280d0834ed5df20599685b7af44773f3
Marco Duarte <marcoaurelio_db@hotmail.com>
2025-09-21 11:26:09 -0300

    feat: update boss flow

--- app/controllers/bosses_controller.rb
+++ app/controllers/bosses_controller.rb
@@ -3,17 +3,31 @@ class BossesController < ApplicationController
   before_action :set_boss, only: [ :show, :update, :destroy ]
 
   def index
-    @bosses = policy_scope(Boss)
+    @bosses = policy_scope(Boss).select(
+                                  "bosses.*",
+                                  "chapters.title as chapter_title",
+                                )
+                                .joins(:chapter)
+                                .order(created_at: :desc)
     render json: @bosses
   end
 
   def show
+    authorize @boss
     render json: @boss
   end
 
   def create
-    @boss = Boss.new(boss_params)
+    chapter = Chapter.find_by(id: create_boss_params[:chapter_id])
+
+    if chapter.nil? || chapter.quest.guild_id != current_user.managed_guild&.id
+      render json: { error: "Chapter não pertence à sua guild" }, status: :unprocessable_entity
+      return
+    end
+
+    @boss = Boss.new(create_boss_params)
     authorize @boss
+
     if @boss.save
       render json: @boss, status: :created
     else
@@ -23,7 +37,7 @@ class BossesController < ApplicationController
 
   def update
     authorize @boss
-    if @boss.update(boss_params)
+    if @boss.update(update_boss_params)
       render json: @boss
     else
       render json: @boss.errors, status: :unprocessable_entity
@@ -42,7 +56,11 @@ class BossesController < ApplicationController
     @boss = Boss.find(params[:id])
   end
 
-  def boss_params
-    params.require(:boss).permit(:name, :description, :chapter_id, :image)
+  def create_boss_params
+    params.require(:boss).permit(:name, :description, :slogan, :chapter_id, :image, :reward_description)
+  end
+
+  def update_boss_params
+    params.require(:boss).permit(:name, :description, :slogan, :image, :reward_description)
   end
 end
--- app/policies/boss_policy.rb
+++ app/policies/boss_policy.rb
@@ -1,21 +1,42 @@
 class BossPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      return scope.none unless user.narrator?
+
+      if user.managed_guild.present?
+        scope.joins(chapter: :quest)
+             .where(quests: { guild_id: user.managed_guild.id })
+      else
+        scope.none
+      end
+    end
+  end
+
   def index?
     user.narrator?
   end
 
   def show?
-    user.narrator?
+    user.narrator? && boss_belongs_to_managed_guild?
   end
 
   def create?
-    user.narrator?
+    user.narrator? && user.managed_guild.present?
   end
 
   def update?
-    user.narrator?
+    user.narrator? && boss_belongs_to_managed_guild?
   end
 
   def destroy?
-    user.narrator?
+    user.narrator? && boss_belongs_to_managed_guild?
+  end
+
+  private
+
+  def boss_belongs_to_managed_guild?
+    return false unless user.managed_guild.present?
+
+    record.chapter.quest.guild_id == user.managed_guild.id
   end
 end
